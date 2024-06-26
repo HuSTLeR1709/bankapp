77641e1ecb35bf9f3ac35c9f8f0d8362ea93836f
Nabhjeet Katiyar <94735685+HuSTLeR1709@users.noreply.github.com>
2024-06-26 13:36:42 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const REACT_APP_BASE_URL = "http://localhost:4000/api/v1";
+const REACT_APP_BASE_URL = "https://bank-app-server.onrender.com/api/v1";
 
 export const transactionEndpoints = {
         DEPOSITE_API : REACT_APP_BASE_URL + "/transaction/deposit",
